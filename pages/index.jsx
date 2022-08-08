8b2e243f5dd662c4704c7ef1c0070166428d5f6b
roxxel <64275777+roxxel@users.noreply.github.com>
2022-08-08 04:22:59 +0300

    aboba

---
+++ pages/index.jsx
@@ -0,0 +1,70 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+import {Input, List, ListItem, SimpleGrid, Spinner, Stack, Text, Image} from '@chakra-ui/react'
+import {useEffect, useState} from "react";
+import { imageHost, getTitles, searchTitles } from '../services/anilibria'
+import {useRouter} from "next/router";
+
+
+export default function Home() {
+  let [isLoading, setIsLoading] = useState(false);
+  let [titles, setTitles] = useState([]);
+  let [searchQuery, setSearchQuery] = useState('')
+  let router = useRouter()
+
+  let searchTimeout;
+
+  const doSearch = (query) => {
+    clearTimeout(searchTimeout)
+    if (!query) return setTitles([]);
+    searchTimeout = setTimeout(() => {
+      router.push({pathName: '/', query: {q: query}}, undefined, {shallow: true})
+    }, 400)
+  }
+
+  useEffect(() => {
+    console.log(router.query.q)
+    setSearchQuery(router.query.q)
+    if (!router.query.q || isLoading) return;
+    setIsLoading(true);
+    searchTitles(router.query.q).then((data) => {
+      console.log(data)
+      if (data.error) {
+        console.log(data.error)
+        return
+      }
+      setTitles(data);
+      setIsLoading(false)
+    })
+  }, [router.query.q])
+  return (
+      <>
+        <Head>
+          <title>Главная | Tangeki</title>
+        </Head>
+        <Stack paddingLeft={[6, 10, 16, 24, 32]} paddingRight={[6, 10, 16, 24, 32]} paddingTop={[6, 10, 16, 24, 48]}>
+          <div style={isLoading ? {display: 'grid', placeItems: 'center'} : {}}>
+            <Input value={searchQuery} onChange={(e) => { setSearchQuery(e.target.value); doSearch(e.target.value) }} placeholder="Поиск" />
+            {
+              isLoading ? <Spinner size='xl' thickness='4px' speed='0.8s' marginTop={4}></Spinner> :
+              <SimpleGrid marginTop={5} spacing={7} minChildWidth='150px'>
+              {
+                titles.map((x) =>
+                  <Stack maxWidth='150px' key={x.id} _hover={{cursor: 'pointer'}} onClick={(e) => router.push({
+                    pathname: '/title',
+                    query: {
+                      id: x.id
+                    }
+                  })}>
+                    <Image  _hover={{transform: 'scale(1.05)'}} style={{borderRadius: '12px', transition: "all 0.25s ease-in-out"}} src={`${imageHost}${x.posters.small.url}`} />
+                    <Text>{x.names.ru}</Text>
+                  </Stack>
+                )
+              }
+            </SimpleGrid>
+            }
+          </div>
+        </Stack>
+      </>
+  )
+}
