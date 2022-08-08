733d05c0f6aa18ba2b21c52ae546576feffcf0ff
roxxel <64275777+roxxel@users.noreply.github.com>
2022-08-08 04:18:54 +0300

    responsive design

--- services/anilibria.js
+++ services/anilibria.js
@@ -2,21 +2,22 @@ const host = "https://api.anilibria.tv/v2/"
 const imageHost = 'https://anilibria.tv'
 
 
-const getTitles = async (idList, codeList, descriptionType = 'plain') => {
+const getTitles = async(idList, codeList, descriptionType = 'plain') => {
     let resp = await fetch(host + 'getTitles?' + new URLSearchParams({
         id_list: (idList || []).join(','),
         code_list: (codeList || []).join(','),
         description_type: descriptionType,
-        playlist_type: 'array'
+        playlist_type: 'array',
     }));
     let data = await resp.json();
     return data;
 }
 
-const searchTitles = async (query) => {
+const searchTitles = async(query) => {
     let resp = await fetch(host + 'searchTitles?' + new URLSearchParams({
         search: query,
-        playlist_type: 'array'
+        playlist_type: 'array',
+        limit: 30
     }));
     let data = await resp.json();
     return data;
